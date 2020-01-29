6bdf5459fc6196163630f4fcab86f0f1c7e48fca
job25721 <amashiikaze@gmail.com>
2020-01-29 11:31:21 +0700

    add api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import '../css/home.css'
+import React from "react";
+import "../css/home.css";
+import api from "../api";
 
 export default class App extends React.Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      apiMsg: "waiting.."
+    };
+    this.getAPI = this.getAPI.bind(this);
+  }
+
+  async getAPI() {
+    const callback = await api.get("/api/testAPI");
+    console.log(callback.data);
+
+    this.setState({ apiMsg: callback.data });
+  }
+
+  render() {
     return (
       <React-fragment>
-      {/* <Helmet>
+        {/* <Helmet>
         <title>ทรงพระเจริญ</title>
       </Helmet> */}
-      <div className="index">
-        <img src="/img/index.jpg" alt="Safty First" className="img-index" />
-        <div className="center">
-          <a href="/">
-              <button className="btn btn-dark button-index"> Enter chat </button>
-          </a>
+        <div className="index">
+          <img src="/img/index.jpg" alt="Safty First" className="img-index" />
+          <div className="center">
+            <a href="/">
+              <button className="btn btn-dark button-index">
+                {" "}
+                Enter chat{" "}
+              </button>
+            </a>
+          </div>
+          <div className="center ">
+            <img className="logo mr-3 ml-3" src="/img/Gear.png" alt="" />
+            &nbsp;&nbsp;&nbsp;
+            <img className="logo mr-3 ml-3" src="/img/cpe.png" alt="" />
+          </div>
+          <p className="center index-text">
+            - Department of Computer Engineering, Chiang Mai University -
+          </p>
+          <div className="text-center">
+            <h1 className="display-4">{this.state.apiMsg}</h1>
+            <button className="btn btn-primary" onClick={this.getAPI}>
+              get API
+            </button>
+          </div>
         </div>
-        <div className="center ">
-          <img className="logo mr-3 ml-3" src="/img/Gear.png" alt="" />
-          &nbsp;&nbsp;&nbsp;
-          <img className="logo mr-3 ml-3" src="/img/cpe.png" alt="" />
-        </div>
-        <p className="center index-text">
-          - Department of Computer Engineering, Chiang Mai University -
-        </p>
-      </div>
-      
-      
-    </React-fragment>
-         
+      </React-fragment>
     );
   }
 }
-
-
-
