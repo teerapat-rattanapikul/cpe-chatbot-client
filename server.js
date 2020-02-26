fb7fbf3d3135aa1cf04ebc1c0b8cf4d48c97c0fc
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2020-02-26 20:04:43 +0700

    remove webhook.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config({ path: 'variables.env' });
 const express = require('express');
 const cors = require('cors');
 const httpStatus = require('http-status');
-const webhookDialogflow = require('./webhook');
 const processMessage = require('./process-message');
 
 const app = express();
@@ -37,14 +36,8 @@ app.post('/api/dialogflowGateway', async (req, res) => {
 
 });
 
-
-app.post('/webhook', async (req, res) => {
-    await webhookDialogflow(req, res);
-});
-
-
 // start server
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Express running → PORT ${server.address()}`);
 });
